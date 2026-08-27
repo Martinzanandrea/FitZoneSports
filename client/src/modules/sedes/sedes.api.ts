e4734469acffee1b85e36347559541bf4ccae57d
martin <martinzanandrea09@gmail.com>
2026-08-27 11:05:42 -0300

    ok

--- client/src/modules/sedes/sedes.api.ts
+++ client/src/modules/sedes/sedes.api.ts
@@ -1,11 +1,13 @@
-import { api } from '../../api/axios';
-import type { Sede, CreateSedePayload } from './sedes.types';
+import { api } from "../../api/axios";
+import type { Sede, CreateSedePayload } from "./sedes.types";
 
 export const sedesApi = {
-  getAll: () => api.get<Sede[]>('/sedes').then((res) => res.data),
+  getAll: () => api.get<Sede[]>("/sedes").then((res) => res.data),
   getOne: (id: string) => api.get<Sede>(`/sedes/${id}`).then((res) => res.data),
   create: (payload: CreateSedePayload) =>
-    api.post<Sede>('/sedes', payload).then((res) => res.data),
-  update: (id: string, payload: Partial<CreateSedePayload> & { activa?: boolean }) =>
-    api.patch<Sede>(`/sedes/${id}`, payload).then((res) => res.data),
+    api.post<Sede>("/sedes", payload).then((res) => res.data),
+  update: (
+    id: string,
+    payload: Partial<CreateSedePayload> & { activa?: boolean },
+  ) => api.patch<Sede>(`/sedes/${id}`, payload).then((res) => res.data),
 };
